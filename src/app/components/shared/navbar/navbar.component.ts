173ba65e13b9a953463897e40dd96e54726ace94
Yan Monasterios <yankevyn02@gmail.com>
2025-05-01 22:34:48 -0400

    add: styles navbar and routerlinkActive

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [
+    RouterLink, RouterLinkActive
+  ],
   templateUrl: './navbar.component.html'
 })
+
+// routerlinkactive permite señalar que a nivel de interfaz nos muestra en que seccion o componente estamos
 export class NavbarComponent {
 
 }
