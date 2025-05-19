c9dfeeb7557c6938f40503fee7d72a2d046139a8
Yan Monasterios <yankevyn02@gmail.com>
2025-05-18 22:37:40 -0400

    changes new about if and for

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal} from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal} from '@angular/core';
 
 interface Character {
   id: number;
@@ -8,18 +9,32 @@ interface Character {
 
 @Component({
   selector: 'app-dragonball',
-  imports: [],
+  imports:[],
   templateUrl: './dragonball.component.html',
   styleUrl: './dragonball.component.css'
 })
 export class DragonballComponent {
 
+  name  = signal('Gohan')
+  power = signal(0)
+
   characters = signal <Character[]> ([
     {id: 1, name: 'Goku', power: 9001},
-    {id: 1, name: 'Vegeta', power: 8000},
-    {id: 1, name: 'Krilin', power: 3005}
+    {id: 2, name: 'Vegeta', power: 8000},
+    {id: 3, name: 'Krilin', power: 3005},
+    {id: 4, name: 'Yamcha', power: 500}
   ]);
 
+  addCharacter() {
+    console.log(this.name(), this.power())
+
+  }
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //   }
+  // }) 
 
 
 }
